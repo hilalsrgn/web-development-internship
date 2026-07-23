2bcdd66bb9324fb0ec0024f200e490572580639a
hilalsrgn <hsrgn05@gmail.com>
2026-07-23 17:30:00 +0300

    day04: JavaScript temelleri, DOM, Fetch API, localStorage

---
+++ day04-javascript/script.js
@@ -0,0 +1,12 @@
+const renkler = ["kırmızı", "mavi", "yeşil", "mor"];
+console.log(renkler[1]);
+renkler.push("sari");
+console.log(renkler);
+
+const urun ={
+    ad:"laptop",
+    fiyat:"5000",
+    stokDurumu : true,
+
+}
+console.log(urun.fiyat);
